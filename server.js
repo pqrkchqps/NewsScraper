20c44d5bb3f64373394cc4f4321ac50f88309b56
thepiratehunter <46298501+thepiratehunter@users.noreply.github.com>
2019-03-21 17:36:37 -0500

    fixing mongo db errors

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
