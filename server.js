4d834f84c619637802753cbd35318f6274b2f29b
thepiratehunter <46298501+thepiratehunter@users.noreply.github.com>
2019-03-21 17:58:50 -0500

    keys

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = keys.MONGODB_URI
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
